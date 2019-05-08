1a33d54731f645e3ba057970125835fbe921374c
okothkongo1 <okothkongo@gmail.com>
2019-05-08 14:35:38 +0300

    set up routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route} from "react-router-dom"
+import Header from './components/Header'
+import Homepage from './components/Homepage'
 import Usersignup from './components/User/Usersignup';
+
 
 function App() {
   return (
-    <div>
-      <Usersignup/>
+    <div >
+      <Router> 
+      <Header/> 
+       <Route exact path="/" component={Homepage} />  
+        <Route path="/user/signup" component={Usersignup} />
+      </Router>     
     </div>
   );
+  
 }
 
 export default App;
