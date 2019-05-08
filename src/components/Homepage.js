7735238c797336ad56c8cc3a7ba9ec7365f2bbf0
okothkongo1 <okothkongo@gmail.com>
2019-05-08 14:33:19 +0300

    add homepage

---
+++ src/components/Homepage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Container} from 'reactstrap';
+
+function Homepage (){
+    return(
+        
+          <Container >
+            <h1 className = 'text-center'>Welcome to Fainda</h1>
+            <p className = 'text-center'>We provide platform to seller and buy your desire</p> 
+        </Container>
+        
+    )
+}
+
+export default Homepage;
